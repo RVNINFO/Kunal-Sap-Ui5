686fa219518f1c65d8a815638a863269c2f4bf5f
shrik <shrii.kunal@gmail.com>
2025-07-08 11:30:58 +0530

    Files Created

---
+++ Project 1 - Feedback Form/project-1-feedback_form/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project1feedbackform/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
--- calculator/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"calculator/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
