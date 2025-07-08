8e207169decadb6a89ccef99136a3d01c5118813
shrik <shrii.kunal@gmail.com>
2025-07-09 00:55:28 +0530

    Project-1 Finished

--- project-1/webapp/controller/View1.controller.js
+++ project-1/webapp/controller/View1.controller.js
@@ -1,10 +1,26 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
-        onInit() {
+      onInit() {},
+      onSubmit: function () {
+        const name = this.byId("nameInput").getValue();
+        const feedback = this.byId("feedbackInput").getValue();
+        if (!name || !feedback) {
+          MessageToast.show("Please fill all fields");
+          return;
         }
+        MessageToast.show("Thank you for your feedback, " + name + "!");
+      },
+      onClear() {
+        const name = this.byId("nameInput");
+        const feedback = this.byId("feedbackInput");
+        if(name) name.setValue('');
+        if(feedback) feedback.setValue('');
+        MessageToast.show("Form Cleared");
+      },
     });
-});
+  }
+);
